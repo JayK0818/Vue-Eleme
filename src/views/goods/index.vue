c7a38b1d3ed5f1dd1e1e96e7803de1565ecc6d88
JayK0818 <1129594783@qq.com>
2023-08-02 21:34:43 +0800

    feat: 商品页面布局

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -17,7 +17,41 @@
         </li>
       </ul>
     </div>
-    <div class="goods-container"></div>
+    <div class="goods-container">
+      <div
+        class="good-category"
+        v-for="good in goods_list"
+        :key="good.name"
+      >
+        <template v-if="good.foods && good.foods.length">
+          <div class="category-title">{{ good.name }}</div>
+          <div class="category-food-container">
+            <div
+              class="food-item border-bottom-1px"
+              v-for="(food, i) in good.foods"
+              :key="food.id"
+              :class="{ 'border-none': i === good.foods.length - 1 }"
+            >
+              <div class="food-img">
+                <img :src="food.image" alt="" class="img">
+              </div>
+              <div class="food-info">
+                <div class="food-title">{{ food.name }}</div>
+                <div class="food-desc" v-if="food.description">{{ food.description }}</div>
+                <div style="padding-top: 8px; line-height:10px;">
+                  <span class="sell-count">月售{{ food.sellCount }}份</span>
+                  <span class="rating-text">好评率{{ food.rating }}%</span>
+                </div>
+                <div class="price">
+                  <span class="new-price">{{ food.price }}</span>
+                  <span class="old-price" v-if="food.oldPrice">{{ food.oldPrice }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -51,11 +85,13 @@ onMounted(() => {
   display: flex;
   height: 100%;
   overflow: auto;
+  background-color: #fff;
   .menu-list {
     box-sizing: border-box;
     padding: 0 12px;
     width: 84px;
     background-color: #f3f5f7;
+    overflow: auto;
     .menu-item {
       padding: 13px 0;
       .menu-text {
@@ -73,6 +109,78 @@ onMounted(() => {
   .goods-container {
     flex: 1;
     min-width: 0;
+    overflow: auto;
+  }
+  .category-title {
+    height: 26px;
+    padding-left: 14px;
+    font-size:12px;
+    line-height: 26px;
+    color: rgb(147, 153, 159);
+    background-color: #f3f5f7;
+    border-left: 1px solid #d9dde1;
+  }
+  .category-food-container {
+    padding: 0 18px;
+    .food-item {
+      padding: 18px 0;
+      display: flex;
+    }
+    .food-img {
+      width: 57px;
+      height: 57px;
+      text-align: center;
+      line-height: 57px;
+      img {
+        max-width: 100%;
+        height: 100%;
+      }
+    }
+    .food-info {
+      padding-left: 10px;
+      flex: 1;
+      min-width: 0;
+      .food-title {
+        padding-top: 2px;
+        font-size: 14px;
+        line-height: 14px;
+        color: rgb(7, 17, 27);
+      }
+      .food-desc, .sell-count, .rating-text {
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+      .rating-text {
+        padding-left: 12px;
+      }
+      .food-desc {
+        line-height: 10px;
+        padding-top: 8px;
+      }
+      .price {
+        line-height: 20px;
+        padding-top: 6px;
+      }
+      .new-price {
+        color: red;
+        font-size: 14px;
+        font-weight: 700;
+      }
+      .new-price:before, .old-price:before {
+        content: '¥';
+        font-weight: normal;
+        font-size: 10px;
+        padding-right: 2px;
+        vertical-align: bottom;
+      }
+      .old-price {
+        margin-left: 8px;
+        text-decoration: line-through;
+        color: rgb(147, 153, 159);
+        font-weight: 700;
+        font-size: 10px;
+      }
+    }
   }
 }
 </style>
